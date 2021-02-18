6bcbaea06ce8390a0ed658f3483f7e55c1801410
Andrei Kisliuk <kisliuk.andrei@gmailcom>
2021-02-18 23:21:27 +0100

    feat: add the script to the dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  // throw new CustomError('Not implemented');
+  // remove line with error and write your code here (['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
+
+  let firstLetters = '';
+  const txtUC = [];
+  if (Array.isArray(members)) {
+    members.forEach(cur => {
+      if (typeof cur === 'string' && cur.length !== 0) {
+        txtUC.push(cur.trim().toUpperCase())
+      }
+    });
+    txtUC.sort();
+
+    txtUC.forEach(cur =>  firstLetters += cur[0]);
+
+    return firstLetters;
+  } 
+
+  return false;
 };
